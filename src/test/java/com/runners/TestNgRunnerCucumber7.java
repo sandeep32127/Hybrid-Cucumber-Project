aa3d243c977121416c350ca1209ecd9446de1bba
Sandeep Kumar Dalei <sandeep32127@gmail.com>
2025-04-19 11:15:32 +0530

    pushing the lastest

--- src/test/java/com/runners/TestNgRunnerCucumber7.java
+++ src/test/java/com/runners/TestNgRunnerCucumber7.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = {"src/test/resources/features"}, glue = {"com.stepdefinitions","com.hooks"},
 plugin = {"pretty","html:Reports/result.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 		 "timeline:test-output-thread/"},
-publish = true)
+publish = true, dryRun = true)
 public class TestNgRunnerCucumber7 extends AbstractTestNGCucumberTests{
 	
 	@Override
